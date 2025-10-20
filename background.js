f0c201fc650ed6052b3ecdec3654dffdeb38bb73
Comayocode <ronalc726@gmail.com>
2025-10-20 00:48:17 -0500

    fix(panel): mostrar al dar clic a la extensión
    Cambiar la ruta para incrustar el panel correctamente en la página

--- background.js
+++ background.js
@@ -2,6 +2,6 @@ chrome.action.onClicked.addListener((tab) => {
   // Renderizar el panel en la página actual
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    files: ["popup/panel.js"]
+    files: ["panel/panel.js"]
   });
 });
